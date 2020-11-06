82f6d06fb24bb62bddacc2412ead570e0e1d53e2
Varun Ananth <varunananth1@gmail.com>
2020-11-06 15:58:06 -0800

    Java Basics Lesson 1 Code

---
+++ BasicJava/IntroToJava/JavaExampleA.java
@@ -0,0 +1,27 @@
+public class JavaExampleA {
+    public static void main (String[] args) {
+        // [primitive type] [name of variable] = [value];
+
+        // [primitive type] [name of variable];
+        // [name of varable] = [value];
+        //String a = "s";
+        int x = 5;
+        boolean isTrue = true;
+        double testDouble = 33.3396;
+        float testFloat = 33.3396f;
+        char testChar = 'A';
+
+        //4
+        double b = (double) 6;
+        char test = (char) 97;
+        System.out.println("Hello World " + x + " dlorW olleH");
+    }
+}
+/*
+int - negative or positive whole numbers
+boolean - true false
+double - positive or negative decimals (up to a certain amount) 45.6666664
+float - positive or negative decimals (up to a certain amount but to a lesser extent than a double) 45.6666664
+char - A a 1 ,<.vlpmkpds
+String - "Hello"
+*/
