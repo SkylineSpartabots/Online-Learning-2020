b3aa94b927e018ee37ca37f163cdc83a48350edc
Varun Ananth <varunananth1@gmail.com>
2020-11-16 11:04:00 -0800

    Boolean problems added for 11/16 meeting

---
+++ BasicJava/IntroToJava/ProjectFiles/BooleanFitProblemTemplate.java
@@ -0,0 +1,9 @@
+package ProjectFiles;
+
+public class BooleanFitProblemTemplate {
+    private static int temp = 50;
+    private static boolean isRaining = true;
+    public static void main(String[] args) {
+
+    }
+}
