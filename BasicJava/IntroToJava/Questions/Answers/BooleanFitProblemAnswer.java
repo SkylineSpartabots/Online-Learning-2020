55aedcddc29e0ce53998ba8e1dcfd27cf9a64286
Varun Ananth <varunananth1@gmail.com>
2020-11-16 15:33:40 -0800

    Quick fix

---
+++ BasicJava/IntroToJava/Questions/Answers/BooleanFitProblemAnswer.java
@@ -0,0 +1,13 @@
+package Questions.Answers;
+
+public class BooleanFitProblemAnswer {
+    private static int temp = 50;
+    private static boolean isRaining = true;
+    public static void main(String[] args) {
+        if(temp <= 70) {
+            System.out.println("Sweatpants");
+        } else if (temp > 70 && temp < 100) {
+            System.out.println();
+        }
+    } 
+}
