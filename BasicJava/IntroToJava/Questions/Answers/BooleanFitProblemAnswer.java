0982c910bde9f9e378d22dd7ebde9b634bd0370d
Varun Ananth <varunananth1@gmail.com>
2020-11-16 16:42:31 -0800

    Answer to BooleanFitProblem uploaded

--- BasicJava/IntroToJava/Questions/Answers/BooleanFitProblemAnswer.java
+++ BasicJava/IntroToJava/Questions/Answers/BooleanFitProblemAnswer.java
@@ -1,9 +1,13 @@
 package Questions.Answers;
 
+
 public class BooleanFitProblemAnswer {
-    private static int temp = 50;
-    private static boolean isRaining = true;
+    private static int temp = 60;
+    private static boolean isRaining = false;
     public static void main(String[] args) {
+        System.out.println("Temp: " + temp);
+        System.out.println("isRaining: " + isRaining + "\n");
+        
         if(temp <= 70) {
             System.out.println("Sweatpants");
         } else if (temp > 70 && temp < 100) {
@@ -15,9 +19,21 @@ public class BooleanFitProblemAnswer {
         if(temp <= 50) {
             System.out.println("Sweatshirt");
         } else if (temp > 50 && temp < 70) {
-            System.out.println("Shorts");
+            System.out.println("T-Shirt");
         } else {
-            System.out.println("Swimsuit");
+            System.out.println("Tank Top");
+        }
+
+        if(temp < 40 || temp > 70) {
+            System.out.println("Hat");
+        } else {
+            System.out.println("No Hat");
+        }
+
+        if(isRaining) {
+            System.out.println("Raincoat");
+        } else {
+            System.out.println("No Raincoat");
         }
     } 
 }
