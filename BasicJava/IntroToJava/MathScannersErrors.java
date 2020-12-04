3b3ed430f9da5e4e0d3e9c68f4e9c6a296364f0a
Varun Ananth <varunananth1@gmail.com>
2020-12-03 21:52:58 -0800

    MathScannersErrors Main + Problems + Project

---
+++ BasicJava/IntroToJava/MathScannersErrors.java
@@ -0,0 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MathScannersErrors {
+    public static void main(String[] args) {
+        //How do we get input?
+
+        //New notation: Object creation -> similar but different to primitive creation
+        //[OBJECT_NAME] [YOUR_OBJECT_NAME] = new [OBJECT_NAME]([PARAMETERS]);
+        /*
+        Scanner methods (think back to string methods)
+            -hasNext() Returns true if the next token matches the pattern constructed from the specified string.
+            -hasNextInt() Returns true if the next token in this scanner's input can be interpreted as an int value in the default radix using the nextInt() method.
+            -hasNextDouble() Returns true if the next token in this scanner's input can be interpreted as a double value using the nextDouble() method.
+            -hasNextBoolean() Returns true if the next token in this scanner's input can be interpreted as a boolean value using a case insensitive pattern created from the string "true|false".
+            
+            -next() Finds and returns the next complete token from this scanner (USES DELIMITERS). String
+            -nextInt() Scans the next token of the input as an int. Int
+            -nextDouble() Scans the next token of the input as a double. Double
+            -nextBoolean() Scans the next token of the input into a boolean value and returns that value. Boolean
+
+            -nextLine() Advances this scanner past the current line and returns the input that was skipped.
+
+            -useDelimiter() Sets this scanner's delimiting pattern to the specified pattern.
+        */
+        //Issues with the specific reads
+        //Try/Catch!
+        //Math operators (+. -. *, /, %)
+    }
+}
---
+++ BasicJava/IntroToJava/ProjectFiles/CalculatorProjectTemplate.java
@@ -0,0 +1,27 @@
+package ProjectFiles;
+
+import java.util.Scanner;
+
+
+public class CalculatorProjectTemplate {
+    public static void main(String[] args) {
+        /*
+        Create a program that can take in an integer, an operator, and another integer (separated by spaces)
+        and return the sum of that operation.
+
+        Example console log:
+        ---
+        Please input your operation: <- computer generated output
+        5 + 5                        <- user input
+        Result: 10                   <- computer generated output
+        Thank you!                   <- computer generated output
+        ---
+
+        Make sure to be able to handle bad input and that your program will not crash. If bad input is given,
+        the program should tell the user that their input is bad.
+
+        Advanced: make a program that can chain together three numbers and possibly handle different operators
+        (dont worry about PEMDAS)
+        */
+    }
+}
